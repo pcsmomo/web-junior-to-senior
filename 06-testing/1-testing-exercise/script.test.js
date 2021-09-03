acf74eb205a861d819dafc3ab93b04e4632f262d
pcsmomo <dongwoo_333@hanmail.net>
2021-09-04 09:06:42 +1000

    173. Writing Tests

--- 06-testing/1-testing-exercise/script.test.js
+++ 06-testing/1-testing-exercise/script.test.js
@@ -2,15 +2,29 @@ const googleSearch = require('./script');
 
 dbMock = ['dogs.com', 'cheesepuff.com', 'disney.com', 'dogpictures.com'];
 
-it('silly test', () => {
-  expect('hello').toBe('hello');
-});
+describe('googleSearch', () => {
+  it('silly test', () => {
+    expect('hello').toBe('hello');
+  });
 
-it('is running', () => {
-  expect(googleSearch('dog', dbMock)).toExist;
-});
+  it('is running', () => {
+    expect(googleSearch('dog', dbMock)).toExist;
+  });
 
-it('is searching google', () => {
-  expect(googleSearch('testtest', dbMock)).toEqual([]);
-  expect(googleSearch('dog', dbMock)).toEqual(['dogs.com', 'dogpictures.com']);
+  it('is searching google', () => {
+    expect(googleSearch('testtest', dbMock)).toEqual([]);
+    expect(googleSearch('dog', dbMock)).toEqual([
+      'dogs.com',
+      'dogpictures.com'
+    ]);
+  });
+
+  it('works with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it('does not return more than 3 matches', () => {
+    expect(googleSearch('.com', dbMock).length).toEqual(3);
+  });
 });
