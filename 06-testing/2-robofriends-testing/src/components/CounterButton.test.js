e592ed971929e61fbf5657b98e1cb54c7b37c98e
pcsmomo <dongwoo_333@hanmail.net>
2021-09-05 08:26:52 +1000

    188. Testing Stateful Components

--- 06-testing/2-robofriends-testing/src/components/CounterButton.test.js
+++ 06-testing/2-robofriends-testing/src/components/CounterButton.test.js
@@ -7,11 +7,15 @@ it('renders without crashing', () => {
 });
 
 it('correnctly increments the counter', () => {
-  const wrapper = shallow(<CounterButton/>)
-  expect(wrapper).toMatchSnapshot();
+  const mockColor = 'red';
+  const wrapper = shallow(<CounterButton color={mockColor} />);
+  
   wrapper.find('[id="counter"]').simulate('click');
   expect((wrapper.state())).toEqual({count: 1})
   wrapper.find('[id="counter"]').simulate('click');
   wrapper.find('[id="counter"]').simulate('click');
   expect((wrapper.state())).toEqual({count: 3})
+  wrapper.find('[id="counter"]').simulate('keypress'); // there no keypress event
+  expect(wrapper.state()).toEqual({ count: 3 });
+  expect(wrapper.props().color).toEqual('red');
 });
