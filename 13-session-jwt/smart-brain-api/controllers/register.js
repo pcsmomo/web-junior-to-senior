9d502095452c98594680c964f17f6da31690787b
pcsmomo <dongwoo_333@hanmail.net>
2021-09-19 08:44:36 +1000

    prettify

--- 13-session-jwt/smart-brain-api/controllers/register.js
+++ 13-session-jwt/smart-brain-api/controllers/register.js
@@ -4,14 +4,15 @@ const handleRegister = (req, res, db, bcrypt) => {
     return res.status(400).json('incorrect form submission');
   }
   const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
+  db.transaction((trx) => {
+    trx
+      .insert({
         hash: hash,
         email: email
       })
       .into('login')
       .returning('email')
-      .then(loginEmail => {
+      .then((loginEmail) => {
         return trx('users')
           .returning('*')
           .insert({
@@ -19,18 +20,15 @@ const handleRegister = (req, res, db, bcrypt) => {
             name: name,
             joined: new Date()
           })
-          .then(user => {
+          .then((user) => {
             res.json(user[0]);
-          })
+          });
       })
       .then(trx.commit)
-      .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
-}
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json('unable to register'));
+};
 
 module.exports = {
   handleRegister: handleRegister
 };
-
-
