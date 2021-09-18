24891bd06b6d37465a956c2d343cfe73d7675d14
pcsmomo <dongwoo_333@hanmail.net>
2021-09-18 13:13:16 +1000

    339. Profile Dropdown

--- 13-session-jwt/smart-brain/src/components/Profile/ProfileIcon.js
+++ 13-session-jwt/smart-brain/src/components/Profile/ProfileIcon.js
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
 
-class ProfileIcon extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dropdownOpen: false
-    };
-  }
+const ProfileIcon = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
-  render() {
-    return (
-      <div className="pa4 tc">
-        <img
-          src="http://tachyons.io/img/logo.jpg"
-          className="br-100 ba h3 w3 dib"
-          alt="avatar"
-        />
-      </div>
-    );
-  }
-}
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+
+  return (
+    <div className="pa4 tc">
+      <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+        <DropdownToggle
+          tag="span"
+          data-toggle="dropdown"
+          aria-expanded={dropdownOpen}
+        >
+          <img
+            src="http://tachyons.io/img/logo.jpg"
+            className="br-100 ba h3 w3 dib"
+            alt="avatar"
+          />
+        </DropdownToggle>
+        <DropdownMenu
+          className="b--transparent shadow-5"
+          style={{
+            marginTop: '20px',
+            backgroundColor: 'rgba(255, 255, 255, 0.5'
+          }}
+        >
+          <DropdownItem>View Profile</DropdownItem>
+          <DropdownItem>Signout</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </div>
+  );
+};
 
 export default ProfileIcon;
