e2d3c50eb1fbed66f0505e7c0b27c00acfc6f74e
pcsmomo <dongwoo_333@hanmail.net>
2021-09-22 16:40:25 +1000

    381. Deploying A Function 2

--- 14-aws/rankly/handler.js
+++ 14-aws/rankly/handler.js
@@ -1,12 +1,17 @@
 'use strict';
 
+const emojis = ['😄', '😃', '😀', '😊', '😉', '😍', '🔶', '🔷', '🚀'];
+
 module.exports.rank = async (event) => {
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank > emojis.length ? emojis.length - 1 : rank];
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event
+        input: rankEmoji
       },
       null,
       2
