91587f72f76d47155cb957fe0479d6e8364fe5d4
pcsmomo <dongwoo_333@hanmail.net>
2021-09-22 17:01:41 +1000

    383. Quick Bug Fix

--- 14-aws/rankly/handler.js
+++ 14-aws/rankly/handler.js
@@ -4,7 +4,7 @@ const emojis = ['😄', '😃', '😀', '😊', '😉', '😍', '🔶', '🔷', 
 
 module.exports.rank = async (event) => {
   const rank = event.queryStringParameters.rank;
-  const rankEmoji = emojis[rank > emojis.length ? emojis.length - 1 : rank];
+  const rankEmoji = emojis[rank >= emojis.length ? emojis.length - 1 : rank];
 
   return {
     statusCode: 200,
